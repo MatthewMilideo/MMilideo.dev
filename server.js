bb6a6553786ce98eb7286a483f3d78ce3a6076b6
Matthew <mmilideo@gmail.com>
2019-10-30 16:54:08 -0400

    Moved Client to one folder

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
 
 // Serve the static files from the React app
-app.use(express.static(path.join(__dirname, '/build')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 // Handles any requests that don't match the ones above
 app.get('*', (req,res) =>{
-    res.sendFile(path.join(__dirname+'/build/index.html'));
+    res.sendFile(path.join(__dirname+'client/build/index.html'));
 });
 
 const port = process.env.PORT || 5000;
