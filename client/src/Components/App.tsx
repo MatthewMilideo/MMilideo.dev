432a3dd631cea71bdb6335906a4b1f36edebb11c
Matthew <mmilideo@gmail.com>
2019-11-01 11:18:00 -0400

    Preparing to push to production

--- client/src/Components/App.tsx
+++ client/src/Components/App.tsx
@@ -233,7 +233,7 @@ function App() {
 	console.log(size, mobile);
 
 	return (
-		<Router basename="/journal">
+		<Router>
 			{mobile ? null : (
 				<React.Fragment>
 					<MainNav linkText={locations} mobile={mobile} />
